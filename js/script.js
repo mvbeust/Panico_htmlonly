8c32780610a811f4df902980ae457688c84c1ed8
Max v. Beust <maxvonbeust@gmail.com>
2016-08-17 00:49:14 +0200

    Submit with autocomplete

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 (function() {
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyB2ycmW4sCSMm6py_NGdjtE77CGFM2PvGQ",
-        authDomain: "project-985851437142041413.firebaseapp.com",
-        databaseURL: "https://project-985851437142041413.firebaseio.com",
-        storageBucket: "project-985851437142041413.appspot.com",
-    };
-    firebase.initializeApp(config);
+
 }());
 
 function TippenPageBuilder() {
+    //Build page for Main Tippen Site
     pageBuilder();
     CurrentTime = Math.floor(Date.now());
     //Get Elements from html
@@ -25,11 +19,13 @@ function TippenPageBuilder() {
         CurrentGames = snapshot.val();
         console.log(CurrentTime);
         console.log(CurrentGames);
+        createformTippen(CurrentGames, CurrentTime)
     });
     makeReady();
 }
 
 function AddGamesPageBuilder() {
+    //Build page for adding new games
     pageBuilder();
     //Get Elements from html
     tableGamesinDB = document.getElementById('GamesinDB');
@@ -53,6 +49,7 @@ function AddGamesbuildingHTML(allGames) {
         "mannschaft1": "text",
         "mannschaft2": "text",
         "wettbewerb": "text",
+        "land": "text",
         "fortschritt": "text",
         "art": "text",
         "timestamp": "datetime",
@@ -64,13 +61,13 @@ function AddGamesbuildingHTML(allGames) {
     for (var i in formfields) {
         if (i == "length") {
             form += '<div class="input-field col s12"><select multiple id="length"><option value="" disabled selected>Choose Length</option><option value="90">90</option><option value="120">120</option><option value="11er">11er</option></select><label>Length</label></div>'
-        } else if (i == "wettbewerb") {
-            form += '<div class="col s12"><label for="' + i + '">' + i + '</label><input type="' + formfields[i] + '" id="' + i + '" name="' + i + '" /></div>';
         } else if (i == "oldtimestamp") {
-            form += '<div class="col s12 hide"><label for="' + i + '">' + i + '</label><input type="' + formfields[i] + '" id="' + i + '" name="' + i + '" /></div>';
+            form += '<div class="input-field col s12 hide"><input type="' + formfields[i] + '" id="' + i + '" name="' + i + '" /><label for="' + i + '">' + i + '</label></div>';
         } else {
-            form += '<div class="col s12 m6"><label for="' + i + '">' + i + '</label><input type="' + formfields[i] + '" id="' + i + '" name="' + i + '" /></div>';
+            form += '<div class="input-field col s12 m6"><input class="autocomplete" type="' + formfields[i] + '" id="' + i + '" name="' + i + '" required/><label for="' + i + '">' + i + '</label></div>';
         }
+        //Generate list of values for autocompletes
+        autoCompletes(i);
     };
     formNewGame.innerHTML = form;
 
@@ -82,6 +79,7 @@ function AddGamesbuildingHTML(allGames) {
     }
     table += '</tr></thead><tbody>';
 
+    //Request a sorted array of keys (sorted by game kickoff time)
     var sortedTimes = sortingObject('timestamp', allGames);
     for (var i = 0; i < sortedTimes.length; i++) {
         var useID = sortedTimes[i].key;
@@ -109,36 +107,64 @@ function AddGamesbuildingHTML(allGames) {
     }
     table += '</tbody>';
     tableGamesinDB.innerHTML = table;
+    makeReady();
 }
 
-function sortingObject(timestamp, Response) {
-    // First create the array of keys/timestamp so that we can sort it:
+function autoCompletes(inputselection) {
+    //This function gets an input from a form and creates the corresponding autocomplete list
+    var outputSelection = inputselection;
+    if (inputselection) {
+        if (inputselection.substr(0, 10) === "mannschaft") {
+            inputselection = "mannschaft";
+        }
+    }
+    //Autocomplete list is requested from database
+    dbResources = firebase.database().ref('resources/' + inputselection);
+    dbResources.on('value', function(snapshot) {
+        dbResources = snapshot.val();
+        //Required javascript code is generated
+        $('input#' + outputSelection).autocomplete({
+            data: dbResources
+        });
+    });
+}
+
+function sortingObject(timestamp, inputObject) {
+    // Create an array of keys and timestamps that will be sorted
     var sort_array = [];
-    for (var key in Response) {
+    for (var key in inputObject) {
         sort_array.push({
             key: key,
-            timestamp: Response[key].timestamp
+            timestamp: inputObject[key].timestamp
         });
     }
-    // Now sort it:
+    //The array is sorted
     sort_array.sort(function(x, y) {
         return x.timestamp - y.timestamp
     });
+    //The sorted array is returned and the object can then be read out by the sorted arrayd
     return (sort_array);
 }
 
 function editGames(allGames, ID) {
     //insert all values into form from line in table
-    console.log(allGames[ID]);
+    var editLength = [];
+    for (var i in allGames[ID].length) {
+        editLength.push(allGames[ID].length[i]);
+    }
+    console.log(editLength);
     document.getElementById('mannschaft1').value = allGames[ID].mannschaft1;
     document.getElementById('mannschaft2').value = allGames[ID].mannschaft2;
     document.getElementById('wettbewerb').value = allGames[ID].wettbewerb;
     document.getElementById('fortschritt').value = allGames[ID].fortschritt;
     document.getElementById('art').value = allGames[ID].art;
     document.getElementById('timestamp').value = new Date(allGames[ID].timestamp).toString();
-    document.getElementById('oldtimestamp').value = new Date(allGames[ID].timestamp).toString();
+    var timeID = ID.substr(0, 13);
+    timeID = parseInt(timeID);
+    document.getElementById('oldtimestamp').value = new Date(timeID).toString();
     document.getElementById('bonuspunkte').value = allGames[ID].bonuspunkte;
-    document.getElementById('length') = allGames[ID].length;
+    document.getElementById('land').value = allGames[ID].land;
+    //document.getElementById('length').value = ["90"];
 }
 
 function submitNewGame() {
@@ -152,6 +178,7 @@ function submitNewGame() {
     var oldtimestamp = document.getElementById('oldtimestamp').value;
     var bonuspunkte = document.getElementById('bonuspunkte').value;
     var length = document.getElementById('length');
+    var land = document.getElementById('land').value;
 
     //Multipleselect for length is analyzed and converted to an object
     var lengthvalues = '{';
@@ -167,6 +194,7 @@ function submitNewGame() {
     //Create Identification for Object in DB and create timestamp
     oldtimestamp = Date.parse(oldtimestamp);
     datevalue = Date.parse(timestamp);
+    //Check if a game is edited or a new game is created
     if (oldtimestamp > 1) {
         console.log("Editing existing game");
         var identification = oldtimestamp.toString() + mannschaft1.replace(/\W+/g, '') + mannschaft2.replace(/\W+/g, '');
@@ -174,14 +202,18 @@ function submitNewGame() {
         var identification = datevalue.toString() + mannschaft1.replace(/\W+/g, '') + mannschaft2.replace(/\W+/g, '');
     }
 
-
     //Create JSON Object
-    var newEntry = '{  "' + identification + '": {  "art": "' + art + '",        "bonuspunkte": ' + bonuspunkte + ',        "fortschritt": "' + fortschritt + '", "length":' + length + ',        "mannschaft1": "' + mannschaft1 + '",        "mannschaft2": "' + mannschaft2 + '",    "timestamp": ' + datevalue + ',      "wettbewerb": "' + wettbewerb + '"}}';
+    var newEntry = '{  "' + identification + '": {  "art": "' + art + '",        "bonuspunkte": ' + bonuspunkte + ',        "fortschritt": "' + fortschritt + '", "length":' + length + ', "land":"' + land + '",        "mannschaft1": "' + mannschaft1 + '",        "mannschaft2": "' + mannschaft2 + '",    "timestamp": ' + datevalue + ',      "wettbewerb": "' + wettbewerb + '"}}';
     var newEntry = jQuery.parseJSON(newEntry);
-
+    var onComplete = function(error) {
+        if (error) {
+            console.log('Synchronization failed');
+        } else {
+            console.log('Synchronization succeeded');
+        }
+    };
     //Send Object to DB
-    dbRefSpiele.update(newEntry);
-
+    dbRefSpiele.update(newEntry, onComplete);
 };
 
 function showDeleteButton(useID) {
@@ -192,20 +224,73 @@ function showDeleteButton(useID) {
 
 function deleteGame(ID) {
     //Delete Game on AddGames Page
-    dbRefTest = firebase.database().ref('spiele/' + ID);
-    dbRefTest.remove();
+    dbRefRemoveLine = firebase.database().ref('spiele/' + ID);
+    dbRefRemoveLine.remove();
 }
 
+function createformTippen(CurrentGames, CurrentTime) {
+    //Create the form for submitting new tipps
+    formTippenfields = {
+        "mannschaft1": "number",
+        "mannschaft2": "number",
+        "length": "select",
+        "winner": "select"
+    }
+    var table = "";
+    cards = "";
+    //Request a sorted array of keys (sorted by game kickoff time)
+    var sortedTimes = sortingObject('timestamp', CurrentGames);
+    for (var i = 0; i < sortedTimes.length; i++) {
+      var useID = sortedTimes[i].key;
+      var item = CurrentGames[useID];
+        //card is opened
+        cards += '<div class="col s12 m6 niemals"><div class = "card" style = "background-color:#fff" ><div class = "card-content" >';
+        dbWettbewerb = firebase.database().ref();
+        dbWettbewerb.once('value').then(function(snapshot) {
+            dbWettbewerb = snapshot.child('resources/wettbewerb').val();
+            ausgabe = '<span class="badge"><img src="'+ dbWettbewerb[item['wettbewerb']]+'" alt="'+item['wettbewerb']+'" class="responsive-img" style="height:30px"/></span>';
+            console.log(dbWettbewerb);
+        });
+        for (var j in formTippenfields) {
+            if (j == "length") {
+                lengthforTable = item[j];
+                var lengthforTablePrint = '';
+                for (var t in lengthforTable) {
+                    lengthforTablePrint += lengthforTable[t] + '|';
+                }
+                lengthforTablePrint = lengthforTablePrint.substring(0, lengthforTablePrint.length - 1);
+            } else if (j == "timestamp") {
+                table += '<td>' + new Date(item[j]).toLocaleString() + '</td>';
+            } else if (j == "oldtimestamp") {} else {
+                table += '<td>' + item[j] + '</td>';
+            }
+        }
+        passID = "'" + useID + "'";
+    }
+    console.log(cards);
+    formTippen.innerHTML = cards;
+    makeReady();
+
+}
+
+
 function pageBuilder() {
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyB2ycmW4sCSMm6py_NGdjtE77CGFM2PvGQ",
+        authDomain: "project-985851437142041413.firebaseapp.com",
+        databaseURL: "https://project-985851437142041413.firebaseio.com",
+        storageBucket: "project-985851437142041413.appspot.com",
+    };
+    firebase.initializeApp(config);
     //Build all elements of the page
     $("#navigation").load('/inc/navigation.html');
     $("#footer").load('/inc/footer.html');
+    makeReady();
 }
 
-function makeReady(){
-  $(document).ready(function() {
-    console.log('makeReady');
-      $('select').material_select();
-      $(".button-collapse").sideNav();
-  });
+function makeReady() {
+    //Trigger javascript to activate elements for navigation and dropdowns
+    $('select').material_select();
+    $(".button-collapse").sideNav();
 }
